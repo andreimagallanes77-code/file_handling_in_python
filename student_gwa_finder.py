f01b39fdda8317978d717ac243c6ceea0363e2cd
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-04-26 00:45:34 +0800

    Added loop to read each student from the file. Separated student name and GWA for processing.

--- student_gwa_finder.py
+++ student_gwa_finder.py
@@ -5,3 +5,12 @@ class StudentGWAFinder:
 
     def find_highest_gwa(self):
         file = open(self.filename, "r")
+
+        top_student_name = ""
+        top_student_gwa = 999
+
+        for line in file:
+            data = line.strip().split()
+
+            student_name = " ".join(data[:-1])
+            student_gwa = float(data[-1])
