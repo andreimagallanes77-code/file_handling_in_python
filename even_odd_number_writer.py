58e0a350eabacc43e4ebe60f0e00a17a1f120d6f
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-04-25 23:12:10 +0800

    Added a loop to read each number from the file. Checked if the number is even or odd and wrote it to the corresponding output file.

--- even_odd_number_writer.py
+++ even_odd_number_writer.py
@@ -9,3 +9,11 @@ class NumberSeparator:
         source_file = open(self.source_filename, "w")
         even_file = open(self.even_filename, "w")
         odd_file = open(self.odd_filename, "w")
+
+        for number_line in source_file:
+            number_value = int(number_line.strip())
+
+            if number_value % 2 == 0:
+                even_file.write(str(number_value) + "\n")
+            else:
+                odd_file.write(str(number_value) + "\n")
