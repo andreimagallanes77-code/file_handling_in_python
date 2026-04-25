fae510da98158da614a4dd9c05a131e0e94b7009
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-04-25 22:58:48 +0800

    Added file handling to open source file and create even and odd output files

--- even_odd_number_writer.py
+++ even_odd_number_writer.py
@@ -4,3 +4,8 @@ class NumberSeparator:
         self.source_filename = "numbers.txt"
         self.even_filename = "even.txt"
         self.odd_filename = "odd.txt"
+
+    def separate_numbers(self):
+        source_file = open(self.source_filename, "w")
+        even_file = open(self.even_filename, "w")
+        odd_file = open(self.odd_filename, "w")
