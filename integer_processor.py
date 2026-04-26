49680656bbf28a887dd35b4cdc887b99baf9aa4b
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-04-27 02:18:05 +0800

    I started the program to process integers from integers.txt. Opened output files double.txt and triple.txt to store squared and cubed values.

---
+++ integer_processor.py
@@ -0,0 +1,6 @@
+class IntegerProcessor:
+
+    def process_numbers(self):
+        source_file = open("integers.txt", "r")
+        double_file = open("double.txt", "w")
+        triple_file = open("triple.txt", "w")
